a3401cc4ba2d7c30b3f064f7ea7ae9e36903522a
JoelitoCS <159519823+JoelitoCS@users.noreply.github.com>
2026-02-02 16:17:38 +0000

    Rutas de api bien configuradas

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,15 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/students', [StudentController::class, 'index']);
 Route::get('/students/{id}', [StudentController::class, 'soloId']);
+
 Route::get('/series', [SeriesController::class, 'index']);
 Route::get('/series/{id}', [SeriesController::class, 'show']);
+Route::post('/series', [SeriesController::class, 'store']);
+Route::put('/series/{id}', [SeriesController::class, 'update']);
+Route::delete('/series/{id}', [SeriesController::class, 'destroy']);
+
 Route::get('/movies', [MovieController::class, 'index']);
 Route::get('/movies/{id}', [MovieController::class, 'show']);
+Route::post('/movies', [MovieController::class, 'store']);
+Route::put('/movies/{id}', [MovieController::class, 'update']);
+Route::delete('/movies/{id}', [MovieController::class, 'destroy']);
