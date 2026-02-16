5077ee056d75547715c6aad3ba9e779448a9a917
JoelitoCS <159519823+JoelitoCS@users.noreply.github.com>
2026-02-16 15:45:15 +0000

    sesion 2 de teoria, registro hecho

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthController extends Controller
+{
+
+    //Parte A: Validacion de datos y creación de usuario, una vez obetnido la información del request, se valida con el validador de Laravel
+
+    public function register (Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:200',
+            'email' => 'required|string|email|max:150|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'role' => 'required|string|in:admin,user'
+        ]);
+
+        //Parte B: Si la validación falla, se devuelve un error con los mensajes de validación.
+
+        if ($validator -> fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        //Como ya tenemos la validación, podemos crear el usuario con lo que tenga request que es lo que el cliente envió y que ya hemos validado, pero como la contraseña no se puede guardar en texto plano, se encripta con bcrypt antes de guardarla.
+
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => bcrypt($request->get('password')),
+            'role' => $request->get('role')
+        ]);
+
+        //Parte C: Finalmente, se devuelve una respuesta JSON con un mensaje de éxito y los datos del usuario creado.
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user,
+            'message' => 'Usuario registrado correctamente'
+        ], 201);
+
+    }
+
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens,HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -20,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
