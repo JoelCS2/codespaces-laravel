d3afee025fa2ae8df976276963a0243a36e70918
JoelitoCS <159519823+JoelitoCS@users.noreply.github.com>
2026-03-16 16:14:15 +0000

    implementacion admin

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\StudentController;
 use App\Http\Controllers\SeriesController;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\IsUserAuth;
+use App\Http\Middleware\isAdmin;
+
 
 //Rutas publicas
 Route::get('/students', [StudentController::class, 'index']);
@@ -26,17 +29,22 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 //Rutas protegidas
-Route::middleware('auth:api')->group(function () {
+Route::middleware([isUserAuth::class])->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'getUser']);
 
+
+   Route::middleware([isAdmin::class])->group(function () {
+
     Route::post('/series', [SeriesController::class, 'store']);
     Route::put('/series/{id}', [SeriesController::class, 'update']);
     Route::delete('/series/{id}', [SeriesController::class, 'destroy']);
-
     Route::post('/movies', [MovieController::class, 'store']);
     Route::put('/movies/{id}', [MovieController::class, 'update']);
     Route::delete('/movies/{id}', [MovieController::class, 'destroy']);
+
+   });
+
 
 });
